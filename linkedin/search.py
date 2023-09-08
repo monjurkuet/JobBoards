73d3b984f1a76f60ef4e7ad91c8e0fe52a2bddb2
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-08 16:22:10 +0600

    Create class LinkedInJobSearch

--- linkedin/search.py
+++ linkedin/search.py
@@ -1,29 +1,37 @@
 import requests
 
-headers = {
-    'authority': 'www.linkedin.com',
-    'accept': '*/*',
-    'accept-language': 'en-GB,en;q=0.6',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'sec-gpc': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-}
-
-params = {
-    'keywords': 'Forklift Operator',
-    'location': 'United States',
-    'locationId': '',
-    'geoId': '103644278',
-    'f_TPR': 'r86400',
-    'position': 1,
-    'pageNum': 0,
-    'start': 0,
-}
-
-response = requests.get(
-    'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search',
-    params=params,
-    headers=headers,
-)
+class LinkedInJobSearch:
+    def __init__(self):
+        self.headers = {
+            'authority': 'www.linkedin.com',
+            'accept': '*/*',
+            'accept-language': 'en-GB,en;q=0.6',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        }
+    def search_jobs(self, keywords, location, start):
+        params = {
+            'keywords': keywords,
+            'location': location,
+            'locationId': '',
+            'geoId': '103644278',
+            'f_TPR': 'r86400',
+            'start': start,
+        }
+        url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search'
+        response = requests.get(url, params=params, headers=self.headers)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+        
+# Example usage:
+linkedin_search = LinkedInJobSearch()
+jobs_data = linkedin_search.search_jobs('Forklift Operator', 'United States',0)
+if jobs_data:
+    print(jobs_data)
+else:
+    print("Failed to fetch job data.")
