d002d5718c38f91946c01f089304d13c3cca49c4
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-24 03:10:08 +0600

    Update Indeed search

--- indeed/search.py
+++ indeed/search.py
@@ -1,16 +1,17 @@
 import undetected_chromedriver as uc
-import requests
 from bs4 import BeautifulSoup
 import platform
 from datetime import datetime
 import time
 import json
+from selenium.webdriver.common.by import By
 
 # Constants for file paths
 BROWSER_EXECUTABLE_PATH_WINDOWS = "C:\\Users\\muham\\AppData\\Local\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
 BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
-# static urls
-JOB_BASE_URL='https://indeed.com/viewjob?jk='
+
+# Static URLs
+JOB_BASE_URL = 'https://indeed.com/viewjob?jk='
 
 def new_browser():
     options = uc.ChromeOptions()
@@ -29,11 +30,11 @@ def new_browser():
     return driver
 
 def parse_jobs(driver):
-    # Scrapping the Web
-    soup = BeautifulSoup(driver.page_source)
-    d = soup.find('div', attrs={'id': 'mosaic-provider-jobcards'})
+    # Scraping the Web
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     jobs = soup.find_all('div', class_='job_seen_beacon')
     job_list = []
+
     for job in jobs:
         job_id = job.find('a')['id'].split('_')[-1]
         job_title = job.find('span', title=True).text.strip()
@@ -41,37 +42,48 @@ def parse_jobs(driver):
         job_location = job.find('div', class_='companyLocation').text.strip()
         job_posted_time = datetime.now().strftime("%Y-%m-%d")
         job_url = JOB_BASE_URL + job_id
+
         job_info = {
-                'job_title': job_title,
-                'company': company,
-                'job_location': job_location,
-                'job_posted_time': job_posted_time,
-                'job_url': job_url,
-            }
+            'job_title': job_title,
+            'company': company,
+            'job_location': job_location,
+            'job_posted_time': job_posted_time,
+            'job_url': job_url,
+        }
         job_list.append(job_info)
         print(job_info)
+
     return job_list
 
-# Skills and Place of Work
-skill = input('Enter your Skill: ').strip()
-place = input('Enter the location: ').strip()
+def main():
+    # Skills and Place of Work
+    skill = input('Enter your Skill: ').strip()
+    place = input('Enter the location: ').strip()
 
-driver=new_browser()
-url = 'https://www.indeed.com/jobs?q=' + skill + \
-        '&l=' + place + '&fromage=1&sort=date'
-driver.get(url)
-allJobs=[]
+    driver = new_browser()
+    url = f'https://www.indeed.com/jobs?q={skill}&l={place}&fromage=1&sort=date'
+    driver.get(url)
 
-while True:
-    job_list=parse_jobs(driver)
-    allJobs.extend(job_list)
-    if not driver.find_elements('xpath','//a[@aria-label="Next Page"]'):
-        break
-    driver.find_element('xpath','//a[@aria-label="Next Page"]').click()
-    time.sleep(10)
+    allJobs = []
 
-with open('indeedJobs.json', 'w') as fout:
-    json.dump(allJobs , fout)
+    while True:
+        job_list = parse_jobs(driver)
+        allJobs.extend(job_list)
 
-driver.close()
-driver.quit()
+        time.sleep(5)
+        next_page_button = driver.find_elements(By.XPATH, '//a[@aria-label="Next Page"]')
+        if not next_page_button:
+            break
+        
+        driver.execute_script("arguments[0].scrollIntoView();", next_page_button[0])
+        driver.execute_script("arguments[0].click();", next_page_button[0])
+        time.sleep(10)
+
+    with open('indeedJobs.json', 'w') as fout:
+        json.dump(allJobs, fout)
+
+    driver.close()
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
