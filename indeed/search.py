c0f885de21a6cc9c7166978cb91ee310997e42a5
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-23 21:47:48 +0600

    Base crawler

--- indeed/search.py
+++ indeed/search.py
@@ -1,62 +1,55 @@
-import os
-import csv
+import undetected_chromedriver as uc
 import requests
 from bs4 import BeautifulSoup
-
-headers = {
-    "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"}
+import platform
+from datetime import datetime
+# Constants for file paths
+BROWSER_EXECUTABLE_PATH_WINDOWS = "C:\\Users\\muham\\AppData\\Local\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
+# static urls
+JOB_BASE_URL='https://indeed.com/viewjob?jk='
+def new_browser():
+    options = uc.ChromeOptions()
+    caps = options.to_capabilities()
+    caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+    SYSTEM_OS = platform.system()
+    browser_executable_path = (
+        BROWSER_EXECUTABLE_PATH_WINDOWS if SYSTEM_OS == 'Windows' else BROWSER_EXECUTABLE_PATH_LINUX
+    )
+    driver = uc.Chrome(
+        browser_executable_path=browser_executable_path,
+        headless=False,
+        options=options,
+        desired_capabilities=caps
+    )
+    return driver
 
 # Skills and Place of Work
 skill = input('Enter your Skill: ').strip()
 place = input('Enter the location: ').strip()
 no_of_pages = int(input('Enter the #pages to scrape: '))
 
+driver=new_browser()
 
-# Creating the Main Directory
-main_dir = os.getcwd() + '\\'
-if not os.path.exists(main_dir):
-    os.mkdir(main_dir)
-    print('Base Directory Created Successfully.')
+for page in range(no_of_pages):
+    url = 'https://www.indeed.com/jobs?q=' + skill + \
+        '&l=' + place + '&start=' + str(page * 10)+'&fromage=1'
+    driver.get(url)
+    # Scrapping the Web
+    soup = BeautifulSoup(driver.page_source)
+    d = soup.find('div', attrs={'id': 'mosaic-provider-jobcards'})
+    jobs = soup.find_all('div', class_='job_seen_beacon')
+    for job in jobs:
+        job_id = job.find('a')['id'].split('_')[-1]
+        job_title = job.find('span', title=True).text.strip()
+        company = job.find('span', class_='companyName').text.strip()
+        location = job.find('div', class_='companyLocation').text.strip()
+        posted = datetime.now().strftime("%Y-%m-%d")
+        job_link = JOB_BASE_URL + job_id
+        #print([job_title, company, location, posted, job_link])
 
-
-# Name of the CSV File
-file_name = skill.title() + '_' + place.title() + '_Jobs.csv'
-# Path of the CSV File
-file_path = main_dir + file_name
-
-# Writing to the CSV File
-with open(file_path, mode='w') as file:
-    writer = csv.writer(file, delimiter=',', lineterminator='\n')
-    # Adding the Column Names to the CSV File
-    writer.writerow(
-        ['JOB_NAME', 'COMPANY', 'LOCATION', 'POSTED', 'APPLY_LINK'])
-
-    # Requesting and getting the webpage using requests
-    print(f'\nScraping in progress...\n')
-    for page in range(no_of_pages):
-        url = 'https://www.indeed.co.in/jobs?q=' + skill + \
-            '&l=' + place + '&start=' + str(page * 10)
-        response = requests.get(url, headers=headers)
-        html = response.text
-
-        # Scrapping the Web
-        soup = BeautifulSoup(html, 'lxml')
-        base_url = 'https://in.indeed.com/viewjob?jk='
-        d = soup.find('div', attrs={'id': 'mosaic-provider-jobcards'})
-
-        jobs = soup.find_all('a', class_='tapItem')
-
-        for job in jobs:
-            job_id = job['id'].split('_')[-1]
-            job_title = job.find('span', title=True).text.strip()
-            company = job.find('span', class_='companyName').text.strip()
-            location = job.find('div', class_='companyLocation').text.strip()
-            posted = job.find('span', class_='date').text.strip()
-            job_link = base_url + job_id
-            #print([job_title, company, location, posted, job_link])
-
-            # Writing to CSV File
-            writer.writerow(
-                [job_title, company, location.title(), posted, job_link])
+        # Writing to CSV File
+        writer.writerow(
+            [job_title, company, location.title(), posted, job_link])
 
 print(f'Jobs data written to <{file_name}> successfully.')
