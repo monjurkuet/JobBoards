74c64b709e9d73a094a5a6d43d8fe2ba260fea7d
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-10-04 19:36:03 +0600

    Update

--- apollo/search_company.py
+++ apollo/search_company.py
@@ -1,32 +1,38 @@
-import requests
-from config import API_KEY,MONGOURL,HOST,PORT,USERNAME,PASSWORD
+import seleniumwire.undetected_chromedriver as uc
+from seleniumwire.utils import decode
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from config import MONGOURL,HOST,PORT,USERNAME,PASSWORD,APOLLO_EMAIL,APOLLO_PASSWORD
 from pymongo import MongoClient
 from datetime import datetime
 import time 
 import mysql.connector
 from urllib.parse import urlparse
 from tqdm import tqdm
+import json
+
+# Constants for API URLs
+COMPANY_DETAILS_API = 'https://app.apollo.io/api/v1/mixed_companies/search'
+LOGIN_URL='https://app.apollo.io/#/login'
+PEOPLE_SEARCH_URL='https://app.apollo.io/#/people'
+COMPANY_SEARCH_URL='https://app.apollo.io/#/companies'
+# Constants for file paths
+BROWSER_EXECUTABLE_PATH_WINDOWS = "C:\\Users\\muham\\AppData\\Local\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+#BROWSER_EXECUTABLE_PATH_WINDOWS = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
+BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
 
 client = MongoClient(MONGOURL)
 
 db = client['companydatabase']
 collection_apollo_company = db['apollo_company_api']
 
-class ApolloAPIClient:
-    def __init__(self):
-        self.api_key = API_KEY
-        self.base_url = "https://api.apollo.io/v1/organizations/enrich"
-        self.headers = {
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json'
-        }
-    def search_organization_domains(self, domain):
-        data = {
-                "api_key": self.api_key,
-                "domain": domain
-            }
-        response = requests.post(self.base_url, headers=self.headers, json=data)
-        return response
+def login(driver):
+    driver.get(LOGIN_URL)
+    time.sleep(10)
+    driver.find_element('xpath','//input[@name="email"]').send_keys(APOLLO_EMAIL)
+    driver.find_element('xpath','//input[@name="password"]').send_keys(APOLLO_PASSWORD)
+    driver.find_element('xpath','//input[@name="password"]').send_keys(Keys.ENTER)
+    time.sleep(10)
 
 def get_domains():
     # Connect to the MySQL database
@@ -44,27 +50,58 @@ def get_domains():
     cursor.execute(query)
     # Fetch all the distinct values as a list
     domains = [row[0] for row in cursor.fetchall()]
-    domains = [urlparse(row).netloc.replace("www.", "") for row in domains]
     # Close the database connection
     conn.close()
     # Print the list of distinct company LinkedIn URLs
     print(len(domains))
     return domains
 
-Apolloclient = ApolloAPIClient()
+# Create a reusable function for extracting real-time data
+def extract_realtime_data(driver):
+    details_content=None
+    #Fix variables for python
+    null=None
+    true=True
+    false=False
+    for request in driver.requests:
+        if request.response:
+            if COMPANY_DETAILS_API in request.url and request.response.status_code == 200 :
+                details_content = eval(decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity')).decode('utf-8'))
+                if 'organizations' in json.dumps(details_content):
+                    break
+    del driver.requests
+    return details_content['organizations']
 
 domains_mysql=get_domains()
 # Query to retrieve all documents and project only the "base domain" field
-projection = {"base_domain": 1, "_id": 0}
+projection = {"domain": 1, "_id": 0}
 # Retrieve all documents and extract the "base domain" values
-base_domains_mongo = [doc["base_domain"] for doc in collection_apollo_company.find({}, projection)]
+base_domains_mongo = [doc["domain"] for doc in collection_apollo_company.find({}, projection)]
 
 new_domains= list(set(domains_mysql).symmetric_difference(set(base_domains_mongo)))
+print(len(new_domains))
+
+driver=uc.Chrome()
+login(driver)
+driver.get(COMPANY_SEARCH_URL)
+#remove popup
+driver.execute_script("arguments[0].remove();", driver.find_element(By.XPATH, '//div[@role="dialog"]'))
 
 for domain in tqdm(new_domains):
-    response = Apolloclient.search_organization_domains(domain).json()
-    if response:
-            response['updateAt']=datetime.now()
-            collection_apollo_company.update_one({"base_domain":domain}, {'$set': response}, upsert=True)
+    driver.find_element(By.XPATH,'//input[@placeholder="Search Companies..."]').clear()
+    driver.find_element(By.XPATH,'//input[@placeholder="Search Companies..."]').send_keys(domain)
+    driver.find_element(By.XPATH,'//input[@placeholder="Search Companies..."]').send_keys(Keys.ENTER)
+    time.sleep(10)
+    details_content=extract_realtime_data(driver)
+    if details_content:
+        details_content=details_content[0]
+    else:
+        details_content={}
+        details_content['primary_domain']=domain
+    details_content['updateAt']=datetime.now()
+    if details_content['primary_domain']:
+        if domain.lower() in details_content['primary_domain'].lower():
+            collection_apollo_company.update_one({"domain":domain}, {'$set': details_content}, upsert=True)
+    else:
+        collection_apollo_company.update_one({"domain":domain}, {'$set': {}}, upsert=True)
     print(f'Crawled : {domain}')
-    time.sleep(1)
---
+++ apollo/search_people.py
@@ -0,0 +1,111 @@
+import seleniumwire.undetected_chromedriver as uc
+from seleniumwire.utils import decode
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from config import MONGOURL,HOST,PORT,USERNAME,PASSWORD,APOLLO_EMAIL,APOLLO_PASSWORD
+from pymongo import MongoClient
+from datetime import datetime
+import time 
+import mysql.connector
+from urllib.parse import urlparse
+from tqdm import tqdm
+import json
+
+# Constants for API URLs
+PEOPE_SEARCH_API = 'https://app.apollo.io/api/v1/mixed_people/search'
+LOGIN_URL='https://app.apollo.io/#/login'
+PEOPLE_SEARCH_URL='https://app.apollo.io/#/people'
+# Constants for file paths
+BROWSER_EXECUTABLE_PATH_WINDOWS = "C:\\Users\\muham\\AppData\\Local\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+#BROWSER_EXECUTABLE_PATH_WINDOWS = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
+BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
+
+client = MongoClient(MONGOURL)
+
+db = client['companydatabase']
+collection_apollo_employee = db['apollo_employee_api']
+collection_apollo_company = db['apollo_company_api']
+
+def get_domains():
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(
+        host=HOST,
+        user=USERNAME,
+        password=PASSWORD,
+        database='jobboards',
+        port=PORT
+        )
+    # Create a cursor object to execute SQL statements
+    cursor = conn.cursor()
+    # Execute the SQL query to retrieve distinct values
+    query = "SELECT domain FROM linkedincrawler_company WHERE domain is not NULL and DATE(updatedAt)=CURDATE()"
+    cursor.execute(query)
+    # Fetch all the distinct values as a list
+    domains = [row[0] for row in cursor.fetchall()]
+    # Close the database connection
+    conn.close()
+    # Print the list of distinct company LinkedIn URLs
+    print(len(domains))
+    return domains
+
+def login(driver):
+    driver.get(LOGIN_URL)
+    time.sleep(10)
+    driver.find_element('xpath','//input[@name="email"]').send_keys(APOLLO_EMAIL)
+    driver.find_element('xpath','//input[@name="password"]').send_keys(APOLLO_PASSWORD)
+    driver.find_element('xpath','//input[@name="password"]').send_keys(Keys.ENTER)
+    time.sleep(10)
+
+def extract_realtime_data(driver):
+    details_content=None
+    #Fix variables for python
+    null=None
+    true=True
+    false=False
+    final_list=[]
+    if driver.requests:
+        for request in driver.requests:
+            if request.response:
+                if PEOPE_SEARCH_API in request.url and request.response.status_code == 200 :
+                    details_content = eval(decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity')).decode('utf-8'))
+                    if 'people' in json.dumps(details_content):
+                        break
+        if details_content:
+            for each_people in details_content['people']+details_content['contacts']:
+                final_list.append(each_people)
+        del driver.requests
+    return final_list
+
+domains_mysql=get_domains()
+
+driver=uc.Chrome()
+login(driver)
+PERSONA_URL='?qPersonPersonaIds[]=651d5c5c32cfc500a3a07707'
+EMAIL_FILTER='&contactEmailStatus[]=verified'
+driver.get(PEOPLE_SEARCH_URL+PERSONA_URL+EMAIL_FILTER)
+#remove popup
+driver.execute_script("arguments[0].remove();", driver.find_element(By.XPATH, '//div[@role="dialog"]'))
+del driver.requests
+
+for domain in tqdm(domains_mysql[5:]):
+    driver.find_element(By.XPATH,'//input[@placeholder="Search People..."]').clear()
+    driver.find_element(By.XPATH,'//input[@placeholder="Search People..."]').send_keys(domain)
+    driver.find_element(By.XPATH,'//input[@placeholder="Search People..."]').send_keys(Keys.ENTER)
+    time.sleep(10)
+    PAGINATION=0
+    while PAGINATION<4:
+        details_content=extract_realtime_data(driver)
+        if details_content:
+            for each_people in details_content:
+                each_people['updateAt']=datetime.now()
+                each_people['domain']=domain.lower()
+                collection_apollo_employee.update_one({"id":each_people['id']}, {'$set': each_people}, upsert=True)
+        print(f'Crawled : {domain}')
+        if not driver.find_elements(By.XPATH,'//button[@aria-label="right-arrow"]'):
+            break
+        if not driver.find_element(By.XPATH,'//button[@aria-label="right-arrow"]').is_enabled():
+            break
+        driver.find_element(By.XPATH,'//button[@aria-label="right-arrow"]').click()
+        time.sleep(10)
+        PAGINATION+=1
+    
